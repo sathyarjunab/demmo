519ee248c352aa4dc5bbc73ac7b6a94915ab52a5
sathyarjunab <sathyarjun007@gmail.com>
2024-08-03 01:01:05 +0530

    clean up

--- gh.js
+++ gh.js
@@ -1,41 +1,5 @@
 const http = require("http");
-const fs = require("fs");
-let pack;
-const server = http.createServer((req, res) => {
-  const method = req.method;
-  if (req.url === "/") {
-    res.setHeader("content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>some name</title></head>");
-    res.write(
-      `<body><form action='/message' method='POST'>${pack}<br><input type="text" name='message'><button type='submit'>submit</button></form></body>`
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (req.url === "/message" && method === "POST") {
-    const data = [];
-    req.on("data", (chunk) => {
-      data.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedData = Buffer.concat(data).toString();
-      let message = parsedData.split("=")[1];
-      fs.writeFile("info.txt", `${message}`, (err) => {
-        pack = message;
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader("content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>some name</title></head>");
-  res.write("<body>hello world</body>");
-  res.write("</html>");
-});
+const routes = require("./routes");
+const server = http.createServer(routes);
 
 server.listen(3000);
