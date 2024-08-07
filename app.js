7faeaea3d583e635f25fadb9427b2c562bda32a3
sathyarjunab <sathyarjun007@gmail.com>
2024-08-07 23:10:59 +0530

    connected db

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
+const db = require("./util/database");
 
 const app = express();
 
@@ -15,6 +16,14 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+db.execute("SELECT * FROM products")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -24,5 +33,5 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 app.listen(port, () => {
-  console.log("FUCKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK");
+  console.log("connected");
 });
